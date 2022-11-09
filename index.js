15dab8f88237b50413eb65e2bbabd5b77847bec1
BmRahman <bmrahman23@gmail.com>
2022-11-09 02:30:41 -0800

    service data post created

--- index.js
+++ index.js
@@ -25,11 +25,24 @@ async function run() {
             res.send(services)
         })
 
-        app.get('/services/:id', async(req, res) => {
+        app.get('/servicelimit', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const servicelimit = await cursor.limit(3).toArray();
+            res.send(servicelimit)
+        })
+
+        app.get('/service/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectID(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service)
+        })
+
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service)
+            res.send(result)
         })
     }
     finally{
