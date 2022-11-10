843311e998566ea0f0bdf975377bc6140442da27
BmRahman <bmrahman23@gmail.com>
2022-11-09 21:47:21 -0800

    email query added on reviews api

--- index.js
+++ index.js
@@ -49,11 +49,24 @@ async function run() {
             res.send(result)
         })
 
-        // reviews api
+        // reviews api create
         app.post('/reviews', async(req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result)
+        })
+
+        // get all reviews
+        app.get('/reviews', async(req, res) => {
+            let query = {}
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query)
+            const reviews = await cursor.toArray()
+            res.send(reviews)
         })
     }
     finally{
